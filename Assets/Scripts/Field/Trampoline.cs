9ee0dd92684c5b4ee3a86b82843d6372c0e14379
abe88j <yuutajb3@gmail.com>
2016-02-27 21:24:32 +0900

    implement trampoline

--- Assets/Scripts/Field/Trampoline.cs
+++ Assets/Scripts/Field/Trampoline.cs
@@ -6,55 +6,41 @@ using UniRx.Triggers;
 public class Trampoline : MonoBehaviour
 {
     [SerializeField]
-    GameObject Player;
-    [SerializeField]
     Vector2 velocity;
 
-    BoxCollider2D BoxCollider2D;
+    bool onTop;
     Animator Animator;
-    ObservableStateMachineTrigger ObservableStateMachineTrigger;
     Rigidbody2D _Rigidbody2D;
 
     void Awake()
     {
-        Player = GameObject.Find("Player");
-        BoxCollider2D = GetComponent<BoxCollider2D>();
         Animator = GetComponent<Animator>();
-        ObservableStateMachineTrigger = Animator.GetBehaviour<ObservableStateMachineTrigger>();
-        _Rigidbody2D = Player.GetComponent<Rigidbody2D>();
     }
 
     void Start()
     {
-        //ObservableStateMachineTrigger
-        //    .OnStateEnterAsObservable()
-        //    .Where(x => x.StateInfo.IsName("Base Layer.Idle"))
-        //    .Zip(this.OnTriggerEnter2DAsObservable().Where(x => x.gameObject.tag == "Player"), (a, b) => b)
-        //    .DelayFrame(5)
-        //    .Subscribe(_ =>
-        //    {
-        //        Animator.SetBool("IsRiding", true);
-        //        jvalue\
-        //    });
-
-        //ObservableStateMachineTrigger
-        //    .OnStateUpdateAsObservable()
-        //    .Where(x => x.StateInfo.IsName("Base Layer.Trampoline"));
-        
-        this.UpdateAsObservable()
-            .Subscribe(_ => Debug.Log(_Rigidbody2D.velocity));
-        this.OnTriggerEnter2DAsObservable()
-            .Where(x => x.gameObject.tag == "Player")
-            .DelayFrame(1)
+        this.OnCollisionStay2DAsObservable()
+            .Where(x => onTop)
             .Subscribe(_ => 
             {
-                _Rigidbody2D.AddForce(new Vector2(0, 600));
                 Animator.SetBool("IsRiding", true);
+                _Rigidbody2D = _.gameObject.GetComponent<Rigidbody2D>();
             });
 
-        ObservableStateMachineTrigger
-            .OnStateExitAsObservable()
-            .Where(x => x.StateInfo.IsName("Base Layer.Trampoline"))
-            .Subscribe(_ => Animator.SetBool("IsRiding", false));
+        this.OnTriggerEnter2DAsObservable()
+            .Subscribe(_ => onTop = true);
+
+        this.OnTriggerExit2DAsObservable()
+            .Subscribe(_ => 
+            {
+                onTop = false;
+                Animator.SetBool("IsRiding", false);
+            });
+        
+    }
+
+    void Jump()
+    {
+        _Rigidbody2D.velocity = velocity;
     }
 }
