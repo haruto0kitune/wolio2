148cb54dd7d5942bb8e3fb3d8532fff79381f4bf
abe88j <yuutajb3@gmail.com>
2016-06-03 19:09:46 +0900

    fix JumpingLightAttack

--- Assets/Scripts/Actors/Player/ChildObject/Attacks/NormalAttacks/JumpingAttacks/JumpingLightAttack/PlayerJumpingLightAttack.cs
+++ Assets/Scripts/Actors/Player/ChildObject/Attacks/NormalAttacks/JumpingAttacks/JumpingLightAttack/PlayerJumpingLightAttack.cs
@@ -7,14 +7,24 @@ namespace Wolio.Actor.Player.Attacks.NormalAttacks.JumpingAttacks
 {
     public class PlayerJumpingLightAttack : MonoBehaviour
     {
+        [SerializeField]
+        GameObject Player;
         Animator Animator;
         ObservableStateMachineTrigger ObservableStateMachineTrigger;
         PlayerState PlayerState;
         Rigidbody2D PlayerRigidbody2D;
         Key Key;
         BoxCollider2D BoxCollider2D;
+        [SerializeField]
+        GameObject PlayerJumpingLightAttackHitBox;
         BoxCollider2D HitBox;
+        [SerializeField]
+        GameObject PlayerJumpingLightAttackHurtBox;
         BoxCollider2D HurtBox;
+        [SerializeField]
+        int damageValue;
+        [SerializeField]
+        int hitRecovery;
         [SerializeField]
         int Startup;
         [SerializeField]
@@ -22,14 +32,14 @@ namespace Wolio.Actor.Player.Attacks.NormalAttacks.JumpingAttacks
 
         void Awake()
         {
-            Animator = GameObject.Find("Test").GetComponent<Animator>();
+            Animator = Player.GetComponent<Animator>();
             ObservableStateMachineTrigger = Animator.GetBehaviour<ObservableStateMachineTrigger>();
-            PlayerState = GameObject.Find("Test").GetComponent<PlayerState>();
-            PlayerRigidbody2D = GameObject.Find("Test").GetComponent<Rigidbody2D>();
-            Key = GameObject.Find("Test").GetComponent<Key>();
+            PlayerState = Player.GetComponent<PlayerState>();
+            PlayerRigidbody2D = Player.GetComponent<Rigidbody2D>();
+            Key = Player.GetComponent<Key>();
             BoxCollider2D = GetComponent<BoxCollider2D>();
-            HitBox = GameObject.Find("JumpingLightAttackHitBox").GetComponent<BoxCollider2D>();
-            HurtBox = GameObject.Find("JumpingLightAttackHurtBox").GetComponent<BoxCollider2D>();
+            HitBox = PlayerJumpingLightAttackHitBox.GetComponent<BoxCollider2D>();
+            HurtBox = PlayerJumpingLightAttackHurtBox.GetComponent<BoxCollider2D>();
         }
 
         void Start()
@@ -46,6 +56,16 @@ namespace Wolio.Actor.Player.Attacks.NormalAttacks.JumpingAttacks
             this.ObserveEveryValueChanged(x => Animator.GetBool("IsJumpingLightAttack"))
                 .Where(x => x)
                 .Subscribe(_ => StartCoroutine(Attack()));
+
+            // Damage
+            PlayerJumpingLightAttackHitBox.OnTriggerEnter2DAsObservable()
+                .Where(x => x.gameObject.tag == "Enemy/HurtBox")
+                .ThrottleFirstFrame(hitRecovery)
+                .Subscribe(_ =>
+                {
+                    _.gameObject.GetComponent<DamageManager>().ApplyDamage(damageValue, hitRecovery);
+                    HitBox.enabled = false;
+                });
         }
 
         public IEnumerator Attack()
